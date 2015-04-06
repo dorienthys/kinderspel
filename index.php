3749600c303a1c8197ad31a2f8e095f7c495296e
LauraDeWinter <dw_laura@hotmail.com>
2015-04-06 13:36:49 +0200

    index aangepast - fonts bijgevoegd - css aanpassingen

--- index.php
+++ index.php
@@ -17,8 +17,14 @@
 
 </head>
 <body>
-		
-		<img src="images/mascotte_index.png" alt="kinderspelmascotte" class="indeximg"/>
+		<img src="images/icon_account.png" alt="account icoon" class="icoon"/>
+
+        <div class="banner">
+        <h1>INLOGGEN</h1>
+    
+        </div> 
+
+		<!--<img src="images/mascotte_index.png" alt="kinderspelmascotte" class="indeximg"/>-->
 
 		<a href="login.php"/><button class="indexbtn" id="indexbtn1">Inloggen</button> </a>
 		<a href="register.php"/><button class="indexbtn" id="indexbtn2">Een account aanmaken</button> </a>
