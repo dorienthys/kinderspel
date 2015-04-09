9adf3cb2ea53cac70cf684416b4c5194fe24f8ae
LauraDeWinter <dw_laura@hotmail.com>
2015-04-09 13:36:42 +0200

    challenge pag front-end

--- challenge.php
+++ challenge.php
@@ -10,22 +10,25 @@
 <html lang="nl">
 <head>
 	<meta charset="UTF-8">
-	<title>Challenge starten</title>
+	<title>Challenge kiezen</title>
 	<meta name="description" content="Kinderspel app">
 	<link rel="stylesheet" href="css/reset.css"/>
 	<link rel="stylesheet" href="css/style.css"/>
 </head>
 <body>
-    
+    <!--hamburger menu-->
   <ul class="navigation">
     <img src="images/icon_account.png" alt="profiel" class="profiel"/>
     <li class="nav-item"><a href="index.php">MIJN OPDRACHTEN</a></li>
     <li class="nav-item"><a href="mijnverhalen.php">MIJN VERHALEN</a></li>
     <li class="nav-item"><a href="help.php">HOE WERKT DE APP</a></li>
 </ul>
+    
  
 <input type="checkbox" id="nav-trigger" class="nav-trigger" />
 <label for="nav-trigger"></label>
+    
+    <!--end menu-->
  
 <div class="site-wrap"> 
 
@@ -34,12 +37,38 @@
     
         <div class="verhalen">
             <div id="verhaal1">
-                <img src="images/junlge.png" alt="jungle" class="jungle"/>
+                <img src="images/junlge.png" alt="jungle" class="foto"/>
                 <h1>JEFKE DE AAP IN DE JUNGLE</h1>
-                <img src="images/gratis.png" alt="gratis verhaal" class="gratis"/>
+                <button type="button" class="freebtn" id="btn1" disabled>GRATIS</button>
             </div>
-    
         </div>
+    
+    
+    <div class="verhalen">
+            <div id="verhaal2">
+                <img src="images/junlge.png" alt="jungle" class="foto"/>
+                <h1>KITO HET VARKEN OP DE BOERDERIJ</h1>
+                <button type="button" class="freebtn" id="btn2">F</button>
+            </div>
+    </div>
+    
+    
+    <div class="verhalen">
+            <div id="verhaal3">
+                <img src="images/junlge.png" alt="jungle" class="foto"/>
+                <h1>BILLIE IN DE RUIMTE</h1>
+                <button type="button" class="freebtn" id="btn3">€ 0.99</button>
+            </div>
+    </div>
+    
+    
+    <div class="verhalen">
+            <div id="verhaal4">
+                <img src="images/junlge.png" alt="jungle" class="foto"/>
+                <h1>STEVEN DE STIER IS SPANJE</h1>
+                <button type="button" class="freebtn" id="btn3">€ 0.99</button>
+            </div>
+    </div>
 
        
 </div>     
