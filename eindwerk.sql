711a4c58c80e8829c7ae996d9634d2cbf50ddb8a
LauraDeWinter <dw_laura@hotmail.com>
2015-04-22 10:55:13 +0200

    databank update

--- eindwerk.sql
+++ eindwerk.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Machine: 127.0.0.1
--- Genereertijd: 18 apr 2015 om 12:38
+-- Genereertijd: 22 apr 2015 om 10:54
 -- Serverversie: 5.6.14
 -- PHP-versie: 5.5.6
 
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `tblafbeeldingen` (
   `ScoreLeegImg` text NOT NULL,
   `PrijsImg` text NOT NULL,
   `startImg` text NOT NULL,
+  `Mascotte` text NOT NULL,
   PRIMARY KEY (`AfbeeldingID`),
   KEY `VerhaalID` (`VerhaalID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
@@ -41,9 +42,9 @@ CREATE TABLE IF NOT EXISTS `tblafbeeldingen` (
 -- Gegevens worden uitgevoerd voor tabel `tblafbeeldingen`
 --
 
-INSERT INTO `tblafbeeldingen` (`AfbeeldingID`, `VerhaalID`, `ScoreVolImg`, `ScoreLeegImg`, `PrijsImg`, `startImg`) VALUES
-(1, 1, 'images/jungle.png', 'images/gratis.png', 'images/jungle.png', 'images/jungle.png'),
-(2, 2, 'd', 'd', 'd', 'images/jungle.png');
+INSERT INTO `tblafbeeldingen` (`AfbeeldingID`, `VerhaalID`, `ScoreVolImg`, `ScoreLeegImg`, `PrijsImg`, `startImg`, `Mascotte`) VALUES
+(1, 1, 'images/banaanVol.png', 'images/banaanLeeg.png', 'images/cadeau1.png', 'images/jungle.png', 'images/mascotte.png'),
+(2, 2, 'd', 'd', 'd', 'images/jungle.png', '');
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,15 @@ CREATE TABLE IF NOT EXISTS `tblkinderen` (
   `Geslacht` varchar(1) NOT NULL,
   PRIMARY KEY (`KindID`),
   KEY `OuderID` (`OuderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `tblkinderen`
+--
+
+INSERT INTO `tblkinderen` (`KindID`, `OuderID`, `Naam`, `Voornaam`, `Geboortedatum`, `Afbeelding`, `Geslacht`) VALUES
+(1, 1, 'KindN', 'KindV', '-2000', 'images/cadeau1.png', 'M'),
+(5, 1, 'ertyhujk', 'dfghjkl', '1991', 'images/jungle.png', 'm');
 
 -- --------------------------------------------------------
 
@@ -79,7 +88,14 @@ CREATE TABLE IF NOT EXISTS `tblouders` (
   `Adres` text NOT NULL,
   `Email` text NOT NULL,
   PRIMARY KEY (`OuderID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Gegevens worden uitgevoerd voor tabel `tblouders`
+--
+
+INSERT INTO `tblouders` (`OuderID`, `Naam`, `Voornaam`, `Wachtwoord`, `Geboortedatum`, `Plaats`, `Adres`, `Email`) VALUES
+(1, 'test', 'test', 'f05bb0d9a57b1e5b10db0a8c96afc5c5', '1992-11-05', 'test', 'test', 'test@test.com');
 
 -- --------------------------------------------------------
 
@@ -95,21 +111,42 @@ CREATE TABLE IF NOT EXISTS `tblpaginas` (
   `Wachtwoord` tinyint(1) NOT NULL,
   `Score` tinyint(1) NOT NULL,
   `Omschrijving` text NOT NULL,
+  `Titel` text NOT NULL,
   PRIMARY KEY (`PaginaID`),
   KEY `VerhaalID` (`VerhaalID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
 
 --
 -- Gegevens worden uitgevoerd voor tabel `tblpaginas`
 --
 
-INSERT INTO `tblpaginas` (`PaginaID`, `VerhaalID`, `PaginaNummer`, `Prijs`, `Wachtwoord`, `Score`, `Omschrijving`) VALUES
-(1, 1, 0, 0, 0, 0, 'Ik zoek 10 bananen zoek jij mee?'),
-(2, 1, 1, 0, 1, 0, 'OPDRACHT 1\r\n\r\nOM IN DE JUNGLE TE OVERLEVEN MOET IK EERST WETEN WELKE DIEREN HIER LEVEN. SCHRIJF IN GROTE DRUKLETTERS VIJF VERSCHILLENDE DIEREN OP. VOORZIE PER DIER DAT JE OPSCHRIJFT 1 BLAD;'),
-(3, 1, 2, 0, 0, 1, 'GOED ZO!\r\nJe Hebt 1 Banaan gevonden.'),
-(4, 1, 3, 0, 1, 0, 'OPDRACHT 2\r\n\r\nKNIP ALLE LETTERS DIE JE NET HEBT GESCHREVEN UIT EN VERSIER ELKE LETTER IN HET THEMA VAN HET DIER.'),
-(5, 1, 4, 0, 0, 1, 'GOED ZO!\r\n\r\nJE HEBT NOG EEN BANAAN GEVONDEN.\r\n\r\nLEVEL 1'),
-(6, 1, 5, 1, 0, 0, 'WAUW! AL LEVEL 1. OMDAT JE ZO GOED HEBT GEHOLPEN BIJ HET VINDEN VAN DE EERSTE BANANEN GEEF IK JOU EEN SUPER COOL DING.');
+INSERT INTO `tblpaginas` (`PaginaID`, `VerhaalID`, `PaginaNummer`, `Prijs`, `Wachtwoord`, `Score`, `Omschrijving`, `Titel`) VALUES
+(1, 1, 0, 0, 0, 0, 'IK BEN JEFKE DE AAP EN IK WIL GRAAG IN DEZE MOOIE JUNGLE WILLEN WONEN. DAAROM IK ZOEK 10 BANANEN, ZOEK JIJ MEE?', 'HALLO '),
+(2, 1, 1, 0, 1, 0, 'OM IN DE JUNGLE TE KUNNEN OVERLEVEN MOET IK EERST WETEN WELKE DIEREN HIER LEVEN. SCHRIJF IN GROTE DRUKLETTERS VIJF VERSCHILLENDE DIEREN OP. GEBRUIK PER DIER 1 BLAD PAPIER.', 'OPDRACHT 1'),
+(3, 1, 2, 0, 0, 1, 'JE HEBT DE EERSTE BANAAN GEVONDEN. LATEN WE VERDER ZOEKEN.', 'GOED ZO!'),
+(4, 1, 3, 0, 1, 0, 'KNIP AL DE DIEREN UIT DIE JE NET HEBT OPGESCHREVEN EN VERSIER ZE ZOALS EEN ECHT DIER.\r\nBIJVOORBEELD: ALS JE SLANG HEBT OPGESCHREVEN KAN JE DIE IN HET GROEN INKLEUREN', 'OPDRACHT 2'),
+(5, 1, 4, 0, 0, 1, 'GOED ZO!\r\n\r\nJE HEBT NOG EEN BANAAN GEVONDEN. HOEVEEL BANANEN HEB JE NU AL?\r\n\r\n', 'LEVEL 1'),
+(6, 1, 5, 1, 0, 0, 'OMDAT JE ZO GOED HEBT GEHOLPEN BIJ HET VINDEN VAN DE EERSTE BANANEN GEEF IK JOU EEN SUPER COOL DING.', 'WAUW AL LEVEL 1.'),
+(7, 1, 6, 0, 1, 0, 'KNUTSEL DE 5 DIEREN NA DIE JE NET HEBT OPGESCHREVEN. JE KAN BIJVOORBEELD EEN DIER NAMAKEN MET PROPPEN WC PAPIER.', 'OPDRACHT 3'),
+(8, 1, 7, 0, 0, 1, 'JE HEBT 3 BANANEN GEVONDEN.', 'GOED ZO!'),
+(9, 1, 8, 0, 1, 0, 'OOH NEE ER ZIJN ENKELE BOMEN WAAR GEEN FRUIT MEER AANGROEIT. LATEN WE SAMEN EEN NIEUWE BOUWEN. NEEM EEN PLASTIEKEN FLES EN MAAK ER EEN BOOMSTAM VAN. TIP: VUL DE FLES MET WAT WATER ZO DAT ZE NIET KAN OMVALLEN.', 'OPDRACHT 4'),
+(10, 1, 9, 0, 0, 1, 'GOED ZO!\r\n\r\nJE HEBT DE VIERDE BANAAN GEVONDEN.', 'LEVEL 2'),
+(11, 1, 10, 1, 0, 0, 'IK DACHT DAT HET NU WEL WAT MOEILIJKER GING WORDEN VOOR JOU. MAAR OMDAT JE AL DE BANANEN TOCH NOG ZO SNEL HEBT GEVONDEN KRIJG JE VAN MIJ IETS SUPER COOL.', 'WAUW DAT IS SNEL!'),
+(12, 1, 11, 0, 1, 0, 'IK BEN MOE. TIJD VOOR EEN KLEINE PAUZE. NEEM EEN BAKJE MET WATER EN LEG EEN 3 SNOEPJES IN. PROBEER DAARNA DE SNOEPJES UIT HET BAKJE TE NEMEN ZONDER JE HANDEN TE GEBRUIKEN.', 'OPDRACHT 5'),
+(13, 1, 12, 0, 0, 1, 'JE HEBT AL 5 BANANEN GEVONDEN. WE ZITTEN AL AAN DE HELFT!', 'GOED ZO!'),
+(14, 1, 13, 0, 1, 0, 'WE MOETEN ZORGEN DAT ONZE BOOM KLAAR IS ZODAT ER ZO SNEL MOGELIJK FRUIT AAN KAN GROEIEN. TEKEN OP EEN BLAD PAPIER VERSCHILLENDE BLADSOORTEN EN KNIP ZE UIT EN VERSIER ZE. INDIEN HET KAN, KAN JE OOK ECHT BLADEREN GAAN ZOEKEN.', 'OPDRACHT 6'),
+(15, 1, 14, 0, 0, 1, 'GOED ZO!\r\n\r\nJE HEBT AL 6 BANANEN GEVONDEN WE ZITTEN OVER DE HELFT.', 'LEVEL 3'),
+(16, 1, 15, 1, 0, 0, 'WAT KAN JIJ MIJ GOED HELPEN. HIER HEB JE NOG EEN CADEAU VAN MIJ VOOR AL JE GOEDE HULP. ', 'SUPER!'),
+(17, 1, 16, 0, 1, 0, 'ER IS EEN BIJENZWERM OP KOMST? BLAAS ZE WEG VOOR MIJ. NEEM 30 KAARTEN EN LEG ZO OP DE BOVENKANT VAN EEN FLES. BLAAS DE KAARTEN ER AF EN ZORG DAT ER MAAR 1 KAART BLIJFT LIGGEN.', 'OPDRACHT 7'),
+(18, 1, 17, 0, 0, 1, 'JE HEBT AL 7 BANANEN GEVONDEN.', 'GOED ZO!'),
+(19, 1, 18, 0, 1, 0, 'ONZE BOOM IS BIJNA AF. NU HEB IK ALLEEN NOG TAKKEN NODIG VOOR AAN DE BOOM. ZOEK RIETJES OF ANDERE MATERIALEN EN MAAK ER TAKJES VAN.', 'OPDRACHT 8'),
+(20, 1, 19, 0, 0, 1, 'GOED ZO!\r\nJE HEBT AL 8 BANANEN GEVONDEN.', 'LEVEL 4 '),
+(21, 1, 20, 1, 0, 0, 'KIJK EENS WAT JE NOG KRIJGT VAN MIJ OMDAT JE MIJ ZO GOED KAN HELPEN.', 'LEVEL 4'),
+(22, 1, 21, 0, 1, 0, 'ALLES VOOR ONZE BOOM IS NU KLAAR. KAN JIJ ALLES AAN ELKAAR KLEVEN ZODAT DE BOOM AF IS?', 'OPDRACHT 9'),
+(23, 1, 22, 0, 0, 1, 'JE HEBT AL 9 BANANEN GEVONDEN. WE ZIJN ER BIJNA PAS DAN KAN IK OVERLEVEN IN DE JUNGLE.', 'GOED ZO!'),
+(24, 1, 23, 0, 1, 0, 'WE HEBBEN BIJNA AL ONZE BANANEN. LEG JIJ DE ANDERE BANANEN AL EVEN WEG. NEEM EEN WASMAND OF SOORT ANDER DOOSJE EN 10 PAAR OPGEVOUWEN SOKKEN. GOOI DE SOKKEN IN DE MAND VAN OP 2 METER AFSTAND.', 'OPDRACHT 10'),
+(25, 1, 24, 0, 0, 1, 'JE HEBT ALLE 10 BANANEN KUNNEN VINDEN.', 'LEVEL 5'),
+(26, 1, 25, 1, 0, 0, 'NU KAN IK DANKZIJ JOU OVERLEVEN IN DE JUNGLE. NU HEB IK ZELFS MIJN EIGEN HUTJE. KOM JE BINNENKORT NOG EENS EEN KIJKJE NEMEN?', 'DANKJEWEL!');
 
 -- --------------------------------------------------------
 
